20cb225658accc9b55310db3ef7b39756f804fd1
Kework K. Kalustian <56342591+KewKalustian@users.noreply.github.com>
2022-01-23 18:37:17 +0100

    Create cleaning.R

---
+++ cleaning.R
@@ -0,0 +1,50 @@
+library(tidyverse)
+library(tidytext)
+library(magrittr)
+library(lsa)
+library(tm)
+
+
+# loading german standard (tm, lsa) and custom stopwords
+
+lsa_stp_wrds <- stopwords_de
+    
+tm_stp_wrds <- stopwords("german")
+
+spec_stp_wrds  <- readRDS("data/stp_wrds/spec_stp_wrds.RDS")
+
+stp_wrds <- tibble(words = unique(c(lsa_stp_wrds, tm_stp_wrds, spec_stp_wrds)))
+
+
+txt <- readLines("data/ring_txt/walküre.txt",encoding = "UTF-8")
+
+####################################
+# cleaning text, string processing #
+####################################
+  
+txt_cleaner <- function(x) {
+
+ x %<>% 
+ # replacing multiple spaces
+ gsub("(?<=[\\s])\\s*|^\\s+|\\s+$", "", ., perl = T) %>% 
+ # "\n" replacing line breaks with space
+ gsub("\n", " ", .) %>%
+ # connecting hyphenated words
+ gsub("-$", "",.) %>% 
+ # as data.frame 
+ data.frame(text = .) %>%
+ # tidy text: 1 word per row
+ unnest_tokens("words", text) %>% 
+ # removing all digits
+ mutate(words = str_remove_all(words, "[[:digit:]]")) %>% 
+ # extracting all words without those ones in the stp_wrds tibble
+ anti_join(stp_wrds, by = "words") %>% 
+ # only using words with more than 3 chars
+ filter(nchar(words, type = "chars") > 3)
+ return(x)
+}   
+
+rheingold <- txt_cleaner(txt)
+
+
+# to be updated
