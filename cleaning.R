5fa442ba179e033256cbda27f51fe4948a2d86b3
Kework K. Kalustian <56342591+KewKalustian@users.noreply.github.com>
2022-01-23 18:40:02 +0100

    Update cleaning.R

--- cleaning.R
+++ cleaning.R
@@ -15,7 +15,7 @@ spec_stp_wrds  <- readRDS("data/stp_wrds/spec_stp_wrds.RDS")
 
 stp_wrds <- tibble(words = unique(c(lsa_stp_wrds, tm_stp_wrds, spec_stp_wrds)))
 
-
+# loading german libretto
 txt <- readLines("data/ring_txt/rheingold.txt",encoding = "UTF-8")
 
 ####################################
